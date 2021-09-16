6f100d41dcb274ab8b01a164667aee85498204e9
xxMattewxx <xxmattewxx@gmail.com>
2021-09-16 17:39:24 -0300

    fix: BOINC uses lower case MD5 values.

--- ChadWrapper/Utils.cs
+++ ChadWrapper/Utils.cs
@@ -42,7 +42,7 @@ namespace ChadWrapper
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < bytes.Length; i++)
             {
-                sb.Append(bytes[i].ToString("X2"));
+                sb.Append(bytes[i].ToString("x2"));
             }
             return sb.ToString();
         }
